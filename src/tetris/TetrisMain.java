35b8dfe6264af1d71c990c75420bdfd64b6f5aeb
ausias <ausias@hINFO6-3.xiam.cat>
2023-01-20 09:49:34 +0100

    primeres funcions

--- src/tetris/TetrisMain.java
+++ src/tetris/TetrisMain.java
@@ -1,20 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tetris;
+import java.util.Scanner;
 
-/**
- *
- * @author ausias
- */
 public class TetrisMain {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        
+        boolean repetir=true;
+        int columnes, files;
+        String[][] taulell;
+        
+        
+        while(repetir==true){
+            System.out.println("Quantes columnes vols que tingui el taulell?");
+            columnes=s.nextInt();
+            System.out.println("Quantes files vols que tingui el taulell?");
+            files=s.nextInt();
+            
+            taulell=TetrisFuncions.CrearTaulell(columnes, files);
+            TetrisFuncions.MostrarTaulell(taulell);
+        }
+        
+        
+        
     }
     
 }
