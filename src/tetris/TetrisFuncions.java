a5d1276b7b9defa383c0323a73873e734ee6f8e3
ausias <ausias@hINFO6-8.xiam.cat>
2023-01-20 10:01:02 +0100

    p1

--- src/tetris/TetrisFuncions.java
+++ src/tetris/TetrisFuncions.java
@@ -4,43 +4,38 @@
  */
 package tetris;
 
-
-
 public class TetrisFuncions {
-    
+
     final static String PARET = "|";
     final static String TERRA = "_";
-    final static String BUIDA ="";
-    
-    static String[][] CrearTaulell(int columnes, int files){
-        
-        String[][] taulell = new String[columnes+2][files+6];
-        
-        for(int i=0; i<columnes+2; i++){
-            for(int j=0; j<files+6; j++){
-                if(i==0 || i==columnes+2){
+    final static String BUIDA = "";
+    final static String PLENA = "X";
+
+    static String[][] CrearTaulell(int columnes, int files) {
+
+        String[][] taulell = new String[columnes + 2][files + 6];
+
+        for (int i = 0; i < columnes + 2; i++) {
+            for (int j = 0; j < files + 6; j++) {
+                if (i == 0 || i == columnes + 2) {
                     taulell[i][j].equals(PARET);
-                }
-                else if(j==files+6 || j==4){
+                } else if (j == files + 6 || j == 4) {
                     taulell[i][j].equals(TERRA);
-                }
-                else{
+                } else {
                     taulell[i][j].equals(BUIDA);
                 }
             }
         }
-        
         return taulell;
     }
-    
-    static void MostrarTaulell(String[][] taulell){
-        for(int i=0; i<columnes+2; i++){
-            for (int j=0; j<files+6; j++){
+
+    static void MostrarTaulell(String[][] taulell, int columnes, int files) {
+        for (int i = 0; i < columnes + 2; i++) {
+            for (int j = 0; j < files + 6; j++) {
                 System.out.println(taulell[i][j]);
             }
             System.out.println("");
         }
     }
-    
-    
+
 }
