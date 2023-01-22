30a471b757e98b6894545ee2bc5462bc12da2538
mique <mique@Ordinador-Miquel>
2023-01-22 20:59:15 +0100

    funcions noves

--- src/tetris/TetrisFuncions.java
+++ src/tetris/TetrisFuncions.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tetris;
+
+import java.util.Scanner;
 
 public class TetrisFuncions {
 
@@ -15,9 +13,9 @@ public class TetrisFuncions {
 
         String[][] taulell = new String[files + 6][columnes + 2];
 
-        for (int i = 0; i < files + 6; i++) {
-            for (int j = 0; j < columnes + 2; j++) {
-                if (i == files + 5 || i == 4) {
+        for (int i = 0; i <= files; i++) {
+            for (int j = 0; j <= columnes + 1; j++) {
+                if (i == files) {
                     taulell[i][j] = TERRA;
                 } else if (j == 0 || j == columnes + 1) {
                     taulell[i][j] = PARET;
@@ -26,16 +24,125 @@ public class TetrisFuncions {
                 }
             }
         }
+
         return taulell;
     }
 
-    static void MostrarTaulell(String[][] taulell, int files, int columnes) {
-        for (int i = 0; i < files + 6; i++) {
-            for (int j = 0; j < columnes + 2; j++) {
+    static String[][] CrearMoviments(int columnes) {
+
+        String[][] moviments = new String[5][columnes + 2];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= columnes + 1; j++) {
+                if (i == 4) {
+                    moviments[i][j] = TERRA;
+                } else if (j == 0 || j == columnes + 1) {
+                    moviments[i][j] = PARET;
+                } else {
+                    moviments[i][j] = BUIDA;
+                }
+            }
+        }
+
+        return moviments;
+    }
+
+    static void MostrarMoviments(String[][] taulell, String[][] moviments, int files, int columnes) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= columnes + 1; j++) {
+                System.out.print(moviments[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void MostrarTaulell(String[][] taulell, String[][] moviments, int files, int columnes) {
+
+        for (int i = 0; i <= files; i++) {
+            for (int j = 0; j <= columnes + 1; j++) {
                 System.out.print(taulell[i][j]);
             }
             System.out.println();
         }
+    }
+
+    static void ImprimirPeca(int peca, String[][] moviments, int columnes) {
+
+        int meitat;
+
+        if (columnes % 2 == 0) {
+            meitat = columnes / 2;
+        } else {
+            meitat = (columnes + 1) / 2;
+        }
+
+        switch (peca) {
+            case 1:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j <= columnes + 1; j++) {
+                        if ((i == 2 && j == meitat) || (i == 2 && j == meitat + 1) || (i == 3 && j == meitat) || (i == 3 && j == meitat + 1)) {
+                            moviments[i][j] = PLENA;
+                        }
+                    }
+                }
+                break;
+            case 2:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j <= columnes + 1; j++) {
+                        if ((i == 2 && j == meitat) || (i == 3 && j == meitat) || (i == 3 && j == meitat + 1) || (i == 3 && j == meitat + 2)) {
+                            moviments[i][j] = PLENA;
+                        }
+                    }
+                }
+                break;
+
+            case 3:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j <= columnes + 1; j++) {
+                        if ((i == 2 && j == meitat + 1) || (i == 3 && j == meitat) || (i == 3 && j == meitat + 1 || (i == 1 && j == meitat + 1))) {
+                            moviments[i][j] = PLENA;
+                        }
+                    }
+                }
+                break;
+
+            case 4:
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j <= columnes + 1; j++) {
+                        if ((i == 0 && j == meitat) || (i == 1 && j == meitat) || (i == 2 && j == meitat) || (i == 3 && j == meitat)) {
+                            moviments[i][j] = PLENA;
+                        }
+                    }
+                }
+                break;
+        }
+
+        MourePeca(peca, moviments, meitat);
+
+    }
+
+    static void MourePeca(int peca, String[][] moviments, int meitat) {
+
+        Scanner s = new Scanner(System.in);
+        String resposta;
+
+        do {
+            System.out.println("Vols moure la peça? (Dreta='a' i Esquerra='d'");
+            System.out.println("Per baixar la peça: 's'");
+            resposta = s.nextLine();
+
+            switch (resposta) {
+                case "a":
+
+            }
+        } while (resposta != "s");
+        BaixarPeca(moviments);
+    }
+
+    static String[][] BaixarPeca(String[][] moviments) {
+
+        return moviments;
     }
 
 }
