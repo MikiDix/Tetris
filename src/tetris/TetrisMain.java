3329984bf0b3bfcdbfe80b086404646ccfa3f7d4
ausias <ausias@hINFO6-3.xiam.cat>
2023-01-23 09:31:08 +0100

    main arreglat

--- src/tetris/TetrisMain.java
+++ src/tetris/TetrisMain.java
@@ -4,21 +4,34 @@ import java.util.Scanner;
 
 public class TetrisMain {
 
+    final static int PECES = 4;
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
         boolean repetir = true;
-        int columnes, files;
-        String[][] taulell;
+        int columnes, files, meitat, peca;
+        String[][] taulell, moviments;
 
-        while (repetir == true) {
+        while (repetir == true && !TetrisFuncions.ComprovarFinal()) {
             System.out.println("Quantes columnes vols que tingui el taulell?");
             columnes = s.nextInt();
             System.out.println("Quantes files vols que tingui el taulell?");
             files = s.nextInt();
 
             taulell = TetrisFuncions.CrearTaulell(files, columnes);
-            TetrisFuncions.MostrarTaulell(taulell, files, columnes);
+            moviments = TetrisFuncions.CrearMoviments(columnes);
+
+            if (columnes % 2 == 0) {
+                meitat = columnes / 2;
+            } else {
+                meitat = (columnes + 1) / 2;
+            }
+
+            while (!TetrisFuncions.ComprovarFinal()) {
+                peca = (int)(Math.random()*PECES+1);
+                TetrisFuncions.ImprimirPeca(peca, moviments, taulell, files, columnes, meitat);
+            }
         }
 
     }
