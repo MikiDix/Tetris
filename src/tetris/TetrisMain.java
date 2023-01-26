e380c87da090808281368c2760bfa7b117d1c04e
ramon <a21rambertor@inspedralbes.cat>
2023-01-26 16:57:47 +0100

    Pregunta 2

--- src/tetris/TetrisMain.java
+++ src/tetris/TetrisMain.java
@@ -26,12 +26,15 @@ public class TetrisMain {
         //fem el bucle fins que ComprovarFinal sigui true 
         while (TetrisFuncions.ComprovarFinal(taulell, meitat) == false) {
 
+            //agafem les columnes i les dividim en 2 per centrar la peça
+            //en cas de que sigui imparell, sumem 1 a les columnes i dividim entre 2
             if (columnes % 2 == 0) {
                 meitat = columnes / 2;
             } else {
                 meitat = (columnes + 1) / 2;
             }
 
+            //generem la peça de forma aleatoria 
             peca = (int) (Math.random() * PECES + 1);
             TetrisFuncions.ImprimirPeca(peca, moviments, taulell, files, columnes, meitat);
             meitat = TetrisFuncions.MourePeca(peca, moviments, taulell, meitat, files, columnes);
